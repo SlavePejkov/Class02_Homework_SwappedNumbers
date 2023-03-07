809d6e2ab5dd57f28b55b0a3ef748dbc7d2b5fab
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2023-03-07 09:42:03 +0100

    Add files via upload

---
+++ SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input two a number");
+
+            string inputOne = Console.ReadLine();           
+            string inputTwo = Console.ReadLine();
+
+            int parsedNumOne;
+            int parsedNumTwo;
+
+            bool parsedResultOne = int.TryParse(inputOne, out parsedNumOne);
+            bool parsedResultTwo = int.TryParse(inputTwo, out parsedNumTwo);
+            
+            Console.WriteLine($"The first number is {parsedNumOne} and the second number is {parsedNumTwo}");
+            
+            parsedNumOne = parsedNumOne + parsedNumTwo;
+            parsedNumTwo = parsedNumOne - parsedNumTwo;
+            parsedNumOne = parsedNumOne - parsedNumTwo;
+
+            Console.WriteLine($"After swapping the first number is {parsedNumOne} and the second number is {parsedNumTwo}");
+        }
+    }
+}
